0aeca24d67b44582ad66ad726e0083033b4d4b88
aaronangxz <angxze@gmail.com>
2021-01-15 22:39:34 +0800

    Updated Lecture01, DataCamp01

---
+++ DataCamp/Intro to Basics/1.7 Basic_data_types_in_R.r
@@ -0,0 +1,17 @@
+#Basic data types in R
+#R works with numerous data types. Some of the most basic types to get started are:
+
+#Decimal values like 4.5 are called numerics.
+#Whole numbers like 4 are called integers. Integers are also numerics.
+#Boolean values (TRUE or FALSE) are called logical.
+#Text (or string) values are called characters.
+#Note how the quotation marks in the editor indicate that "some text" is a string.
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
