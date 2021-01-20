993f7f397a9f8065fe9bb34eb03e02bde80de71b
aaronangxz <angxze@gmail.com>
2021-01-20 23:20:46 +0800

    Updated

--- DataCamp/Vectors/01.Create_a_vactor.r
+++ DataCamp/Vectors/01.Create_a_vactor.r
@@ -3,5 +3,7 @@
 
 #Thanks to R and your new data-analytical skills, you will learn how to uplift your performance at the tables and fire off your career as a professional gambler. This chapter will show how you can easily keep track of your betting progress and how you can do some simple analyses on past actions. Next stop, Vegas Baby… VEGAS!!
 
+##################################################
+
 # Define the variable vegas
 vegas <- "Go!"
